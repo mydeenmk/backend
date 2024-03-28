c37fa0b5eb290e398a28132017bc055aa38661b5
mydeenmk <a.mydeenmohammed@gmail.com>
2024-03-28 17:08:58 +0530

    main

--- Frontend/src/Signin.jsx
+++ Frontend/src/Signin.jsx
@@ -50,7 +50,7 @@ function Signin() {
             password: values.password || undefined,
         }
 try{
-    fetch(' http://localhost:3000/api/users', {
+    fetch(' http://localhost:3000/auth/signin', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
